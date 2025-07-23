d6ade31ab607631ac9afd3512e2cdffa4c4c839f
小海 <3277975910@qq.com>
2025-07-23 21:30:09 +0800

    feat: 添加了pinia来维护知识库列表，更新了搜索筛选的方法

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router';
 import TDesign from 'tdesign-vue-next';
 import './assets/tailwind.css';
-
+import pinia from './store' // 导入 Pinia 实例
 import TDesignChat from '@tdesign-vue-next/chat'; // 引入chat组件
 import 'tdesign-vue-next/es/style/index.css'; // 引入少量全局样式变量
 
@@ -19,4 +19,5 @@ app.config.performance = true
 app.use(TDesignChat);
 app.use(TDesign);
 app.use(router);
+app.use(pinia); // 使用 Pinia 实例
 app.mount('#app');
